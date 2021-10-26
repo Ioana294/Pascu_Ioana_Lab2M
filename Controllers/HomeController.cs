3cd704dbf00c6e999a1e95367a135b4488c077af
Ioana Pașcu <ioana_lz@yahoo.com>
2021-10-26 20:29:41 +0300

    Lab4suplimentar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using Pascu_Ioana_Lab2.Models;
 using Microsoft.EntityFrameworkCore;
 using Pascu_Ioana_Lab2.Data;
 using Pascu_Ioana_Lab2.Models.LibraryViewModels;
+using Pascu_Ioana_Lab2.Models.CustomerViewModels;
 
 namespace Pascu_Ioana_Lab2.Controllers
 {
@@ -51,6 +52,19 @@ namespace Pascu_Ioana_Lab2.Controllers
                 {
                     OrderDate = dateGroup.Key,
                     BookCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+        public async Task<ActionResult> CustomerStatistics()
+        {
+            IQueryable<CustomerGroup> data =
+                from customer in _context.Customers
+                group customer by customer.Name into customerGroup
+                select new CustomerGroup()
+                {
+                    CustomerName = customerGroup.Key,
+                    BookCount = customerGroup.Count()
                 };
             return View(await data.AsNoTracking().ToListAsync());
         }
